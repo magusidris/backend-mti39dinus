63d3592ab3e16194530699207033eb7f4cd19c31
nuxtcraft <magusidris@gmail.com>
2022-02-03 04:26:38 +0700

    add kumpul tugas IP dan IR

---
+++ app/Http/Controllers/Api/Web/KumpulIPController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\KumpulIp as Impros;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Str;
+use App\Http\Resources\KumpulDMResource;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Resources\KumpulIPResource;
+use Illuminate\Http\Request;
+
+class KumpulIPController extends Controller
+{
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'image'           => 'required|mimes:doc,docx,pdf|max:3000',
+            'npm'           => 'required',
+            'name'          => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // upload doc
+        $image = $request->file('image');
+
+        $nim = strtoupper($request->npm);
+        $extname = $image->getClientOriginalExtension();
+        $docname = 'IP_'.$nim.'_Reg-39'.'.'.$extname;
+
+        $image->storeAs('public/kumpulips', $docname);
+
+        // create Photo
+        $tugas = Impros::create([
+            'image'     => $docname,
+            'npm'       => $request->npm,
+            'name'      => $request->name
+        ]);
+
+        if ($tugas) {
+            // return success with Api Resource
+            return new KumpulIPResource(true, 'Tugas Berhasil Disimpan!', $tugas);
+        }
+
+        // return failed with Api Resource
+        return new KumpulIPResource(false, 'Tugas Gagal Disimpan!', null);
+    }
+}
---
+++ app/Http/Controllers/Api/Web/KumpulIRController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\KumpulIr as Infer;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Str;
+use App\Http\Resources\KumpulDMResource;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Resources\KumpulIRResource;
+use Illuminate\Http\Request;
+
+class KumpulIRController extends Controller
+{
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'image'           => 'required|mimes:doc,docx,pdf|max:3000',
+            'npm'           => 'required',
+            'name'          => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // upload doc
+        $image = $request->file('image');
+
+        $nim = strtoupper($request->npm);
+        $extname = $image->getClientOriginalExtension();
+        $docname = 'IR_'.$nim.'_Reg-39'.'.'.$extname;
+
+        $image->storeAs('public/kumpulirs', $docname);
+
+        // create Photo
+        $tugas = Infer::create([
+            'image'     => $docname,
+            'npm'       => $request->npm,
+            'name'      => $request->name
+        ]);
+
+        if ($tugas) {
+            // return success with Api Resource
+            return new KumpulIRResource(true, 'Tugas Berhasil Disimpan!', $tugas);
+        }
+
+        // return failed with Api Resource
+        return new KumpulIRResource(false, 'Tugas Gagal Disimpan!', null);
+    }
+}
